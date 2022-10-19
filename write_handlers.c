56b6aa3c0a285eb9021ff183988b41b5f3b0a37d
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2022-10-19 17:02:34 +0000

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -3,10 +3,10 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * @c: char types.
+ * @c: Char types
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
- * @width: get width.
+ * @width: Get width
  * @precision: precision specifier
  * @size: Size specifier
  *
@@ -48,10 +48,10 @@ int handle_write_char(char c, char buffer[],
 /**
  * write_number - Prints a string
  * @is_negative: Lista of arguments
- * @ind: char types.
+ * @ind: Char types
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width.
+ * @width: Get width
  * @precision: precision specifier
  * @size: Size specifier
  *
@@ -83,7 +83,7 @@ int write_number(int is_negative, int ind, char buffer[],
  * @ind: Index at which the number starts on the buffer
  * @buffer: Buffer
  * @flags: Flags
- * @width: width
+ * @width: Get width
  * @prec: Precision specifier
  * @length: Number length
  * @padd: Pading char
